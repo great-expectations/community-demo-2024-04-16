3b4672c43c95b94c25dfd2167b56aaecbace9bf2
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2024-04-15 10:42:14 -0400

    Pull out more constants and rework flow of expectation authoring

--- demos/constants.py
+++ demos/constants.py
@@ -1,2 +1,9 @@
 DB_CONNECTION_STRING = "postgresql+psycopg2://postgres:password@localhost:5432/demo"
 TABLE_NAME = "yellow_tripdata"
+
+
+DATASOURCE_NAME = "Postgres"
+ASSET_NAME = "Trip Data"
+BATCH_DEFINITION_NAME_PARTIONED = "Partition by Month"
+BATCH_DEFINITION_NAME_WHOLE_TABLE = "My Batch Definition"
+SUITE_NAME = "Passenger Count Checker"
